22bb5e059d7d6acb0192eac75d249caafcac67ba
Carlos A. Peralta Alvarez <cperalta22@users.noreply.github.com>
2022-01-20 08:43:27 -0600

    Update twic_fetch.sh

--- twic_fetch.sh
+++ twic_fetch.sh
@@ -1,6 +1,7 @@
 # provide into terminal arguments how many TWIC releases you want to download
 # this is very basic way to do it, and only fetch from newest to oldest with
 # no other option
+# see https://github.com/cperalta22/chess-database-toolkit/blob/main/README.md for instructions
 
 lynx -listonly -nonumbers -dump  https://theweekinchess.com/twic | grep g.zip | head -n $1 | parallel --gnu "wget {}"
 ls *.zip | parallel --gnu "unzip {}"
